e538cb98b424caa40aeeba92a8189fe6ba6abcb4
Stelios Georgiou <steliosdg@outlook.com>
2020-10-31 20:55:56 +0000

    adjust for new dir structure

--- matchIdSamples/combine_samples.py
+++ matchIdSamples/combine_samples.py
@@ -1,11 +1,14 @@
-import pandas as pd
 import glob
 
+import pandas as pd
+
+date = "27_10_20"
+date_fmt = './' + date + '/'
 batch = []
-for f in glob.glob('./matchIdSamples/*.json'):
-    data = pd.read_json(f).rename(columns={0:'matchId'})
+for f in glob.glob(date_fmt + '*.json'):
+    data = pd.read_json(f).rename(columns={0: 'matchId'})
     data['region'] = f[:-5].split('_')[1]
     batch.append(data)
-    
+
 df = pd.concat(batch, axis=0).reset_index()
-df.to_json('./matchIdSamples/matchlistSamples.json')
+df.to_json(date_fmt + 'matchlistSamples.json')
