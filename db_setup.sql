434f96aa2f7b7a1f9bcc5c781052cc17e8ef2e72
Stelios Georgiou <steliosdg@outlook.com>
2020-11-01 12:17:05 +0000

    added summoner id info (from https://riot-api-libraries.readthedocs.io/en/latest/ids.html)

--- db_setup.sql
+++ db_setup.sql
@@ -1,9 +1,24 @@
 CREATE TABLE summoners
 /*
 persons will be the reference table for summoners and account info
-there mayb be non-static data, such as current ranks and summoner levels, 
+there may be non-static data, such as current ranks and summoner levels,
 but there should be an accompanying revisionDate to give a timestamp that
 is near to when the data was collected.
+
+PUUIDs and Other IDs:
+The Riot API uses three IDs for players: summoner IDs, account IDs, and
+PUUIDs. Different APIs use different IDs, and you should use whichever ID
+is required by the API you are using.
+Summoner and account IDs are only unique per region, and PUUIDs are unique
+globally.
+Because the PUUID is globally unique, when a player transfers regions
+their PUUID will not change. This allows you to track summoners that have
+transferred regions. If a player transfers regions, their summoner and
+account IDs will change.
+All IDs are encrypted using encryption keys unique to each project. An ID
+obtained with your dev key will not work with your production key (and
+vice versa). When you refresh a key, the encrypted ID will not change.
+
 */
 (
     summonerName  character varying(16),
