98cc35e89ba04eac03d5eb63b023d4f6e38663e0
Stelios Georgiou <steliosdg@outlook.com>
2020-11-01 03:51:18 +0000

    Fix error on string split, regions were taking part of the date, not the region string

--- matchIdSamples/combine_samples.py
+++ matchIdSamples/combine_samples.py
@@ -7,7 +7,10 @@ date_fmt = './' + date + '/'
 batch = []
 for f in glob.glob(date_fmt + '*.json'):
     data = pd.read_json(f).rename(columns={0: 'matchId'})
-    data['region'] = f[:-5].split('_')[1]
+    print(f)
+    print(f[:-5].split('_')[-1])
+    # TODO: make this namer more robust
+    data['region'] = f[:-5].split('_')[-1]
     batch.append(data)
 
 df = pd.concat(batch, axis=0).reset_index()
