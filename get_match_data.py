21c6b4f46dcecaba6630b3f7ca4e1f35dc4d09a7
Stelios Georgiou <steliosdg@outlook.com>
2020-11-01 03:57:10 +0000

    now scrapes match and timeline data, saves as json

--- get_match_data.py
+++ get_match_data.py
@@ -1,25 +1,60 @@
 """
 Get match and match timeline data, given a matchId
 """
-from riotwatcher import LolWatcher, ApiError
+import json
+
 import pandas as pd
+from riotwatcher import LolWatcher, ApiError
 
-
-API_KEY = open('API-KEY.txt').read()
+# TODO: Get all match and timeline data, for every matchId
+API_KEY = open('API-KEY').read()
 # TODO: close file?
 
 watcher = LolWatcher(API_KEY)
 
-ids = pd.read_json('matchIdSamples/10_08_20/matchlistSamples.json')
-eu = pd.read_json('matchIdSamples/10_08_20/matchlist_euw1.json')
+ids = pd.read_json('matchIdSamples/27_10_20/matchlistSamples.json')
 
-# game = watcher.match.by_id('euw1', eu.values[0][0])
-# timeline = watcher.match.timeline_by_match('euw1', eu.values[0][0])
-print(eu.values[0])
-print(ids.region[0])
-game = watcher.match.by_id(ids.region[0], ids.matchId[0])
-timeline = watcher.match.timeline_by_match(ids.region[0], ids.matchId[0])
 
-# for n in range(lane(ids)):
-#     game = watcher.match.by_id(ids.region[n], ids.matchId[n])
-    # timeline = game
+# game = watcher.match.by_id(ids.region[0], ids.matchId[0])
+# timeline = watcher.match.timeline_by_match(ids.region[0], ids.matchId[0])
+
+
+def get_match(region, match_id):
+    try:
+        game = watcher.match.by_id(region, match_id)
+        return game
+    except ApiError as err:
+        if err.response.status_code == 429:
+            # The 429 status code indicates that the user has sent too many requests
+            # in a given amount of time ("rate limiting").
+            print('Try in {} seconds.'.format(err.response.headers['Retry-After']))
+            print('this retry-after is handled by default by the RiotWatcher library')
+            print('future requests wait until the retry-after time passes')
+        else:
+            raise
+
+
+def get_timeline(region, match_id):
+    try:
+        game = watcher.match.timeline_by_match(region, match_id)
+        return game
+    except ApiError as err:
+        if err.response.status_code == 429:
+            # The 429 status code indicates that the user has sent too many requests
+            # in a given amount of time ("rate limiting").
+            print('Try in {} seconds.'.format(err.response.headers['Retry-After']))
+            print('this retry-after is handled by default by the RiotWatcher library')
+            print('future requests wait until the retry-after time passes')
+        else:
+            raise
+
+
+for n, m_id in enumerate(ids.matchId.values):
+    match = get_match(ids.region[n], m_id)
+    with open('matches/' + str(m_id) + '.json', 'w') as fp_match:
+        json.dump(match, fp_match, indent=4)
+    fp_match.close()
+    timeline = get_timeline(ids.region[n], m_id)
+    with open('matches/' + str(m_id) + '_timeline.json', 'w') as fp_timeline:
+        json.dump(timeline, fp_timeline, indent=4)
+    fp_timeline.close()
