66986f4ae832dd05e4d551d6f60a89ba1290bc62
kepler471 <steliosdg@gmail.com>
2021-10-06 01:11:33 +0100

    function to get champion win rate data

--- win_rates.py
+++ win_rates.py
@@ -3,7 +3,31 @@ match data, and calculate champion win rates.
 """
 
 from utils.parser import *
+import pandas as pd
 
-CHAMPION_MAP = get_champion_map("ref/champion.json")
 
-match = Match("matchIdSamples/match_4728783388.json")
+def produce_winrates_data(path):
+    match_data = {
+        "champs": [],
+        "team": [],
+        "win": [],
+        "gameId": [],
+    }
+
+    for f in path.glob("*"):
+        # Select only match files
+        if not f.suffix == ".json" or "timeline" in f.stem or "response" in f.stem:
+            continue
+
+        match = Match(f)
+
+        # Append each 10 champions to match_data, along with important info
+        match_data["champs"] += list(match.champs.keys())
+        match_data["team"] += list(match.champs.values())
+        match_data["win"] += [
+            True if match.champs[x] == match.winning_team else False
+            for x in match.champs
+        ]
+        match_data["gameId"] += 10 * [match.data["gameId"]]
+
+    return pd.DataFrame(data=match_data)
